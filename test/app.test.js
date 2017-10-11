ff1829d5c746bee7212e6f4f08b0d4612de3f397
Shane Moyo <smoyo@pdx.edu>
2017-10-11 09:18:41 -0700

    added failing get greeting test

--- test/app.test.js
+++ test/app.test.js
@@ -3,8 +3,18 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
 
-
 const app = require('../lib/app');
 
+const request = chai.request(app);
 
+describe('app test', ()=>{
+    it('GET /greeting/<name> should respond with greeting',(done) => {
+        request.get('/greeting/Shane')
+            .end((err, res) =>{
+                if(err) return done(err);
+                assert.equal(res.text, '<h1>Hello Shane</h1>');
+                done();
+            });
+    });
 
+});
