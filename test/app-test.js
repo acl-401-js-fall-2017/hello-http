148c5a883e8af71c8784136f33f3003b0167df03
Robin Miller <rmillerpdx@gmail.com>
2017-10-11 12:48:48 -0700

    404 test passes

--- test/app-test.js
+++ test/app-test.js
@@ -7,7 +7,7 @@ const app = require('../lib/app');
 //starts a server and makes it so request object talks to server that got started
 const request = chai.request(app);
 
-describe('http greeting', () => {
+describe('http module', () => {
     
     it('GET /greeting returns "hello, stranger" text', done => {
         request.get('/greeting')
@@ -18,7 +18,7 @@ describe('http greeting', () => {
             });
     });
 
-    it.only('GET /greeting/foo returns "hello, foo" text', done => {
+    it('GET /greeting/foo returns "hello, foo" text', done => {
         request.get('/greeting/foo')
             .end( (err, res) => {
                 if (err) return done(err);
@@ -45,8 +45,15 @@ describe('http greeting', () => {
                 done();
             });
     });
+
+    it('returns 404 message on invalid url', (done) => {
+        request.get('/brokenlink')
+            .end( (err, res) => {
+                if (err) {
+                    assert.equal(res.statusCode, 404);
+                }
+                done();
+            });
+    });
     
 });
-
-///respond with a greeting if method is GET and url path is /greeting/<name>
-// on localhost:8080/greeting/ , web text is "hello, stranger"
