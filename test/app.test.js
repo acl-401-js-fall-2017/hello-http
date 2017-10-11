dc6801351526a385bc7bfbd2b68ecb69b7bd21a8
Christina Mills <chrimills@gmail.com>
2017-10-11 11:21:56 -0700

    one working test, one failing

--- test/app.test.js
+++ test/app.test.js
@@ -7,11 +7,22 @@ const app = require('../lib/app');
 const request = chai.request(app);
 
 describe('Tests Http', () => {
-    it('GET request returns greeting', () => {
-        request.get('/greeting/Christina')
+    it('GET request returns DEFAULT greeting', (done) => {
+        request.get('/greeting')
             .end( (err,res) => {
                 if(err) return done(err);
-                assert.equal(res.text, 'Hello Christina');
-            })
-    })  
-})
+                assert.equal(res.text, 'Hello, stranger');
+                done();
+            });
+    });
+
+    it('GET request returns greeting with NAME', (done) => {
+        request.get('greeting/Christina')
+            .end( (err, res) => {
+                if(err) return done(err);
+                assert.equal(res.text, 'Hello, Christina');
+                done();
+            });
+    });
+});
+
