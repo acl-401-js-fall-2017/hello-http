9062db1e228354945442f00758a1d73b4ec09af3
Christina Mills <chrimills@gmail.com>
2017-10-11 13:46:00 -0700

    three tests running

--- test/app.test.js
+++ test/app.test.js
@@ -1,29 +1,44 @@
 const chai = require('chai');
+const http = require('http');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
 const app = require('../lib/app');
 
-const request = chai.request(app);
-let name = null;
+const server = http.createServer(app);
+const request = chai.request(server);
+
+after(done => server.close(done));
+
+
 
 describe('Tests Http', () => {
     it('GET request returns DEFAULT greeting', (done) => {
         request.get('/greeting')
             .end( (err,res) => {
                 if(err) return done(err);
-                assert.equal(res.text, 'Hello stranger');
+                assert.equal(res.text, 'Hello,  stranger');
                 done();
             });
     });
 
     it('GET request returns greeting with NAME', (done) => {
-        request.get(`/greeting/${name}`)
+        request.get('/greeting/Christina')
             .end( (err, res) => {
                 if(err) return done(err);
-                assert.equal(res.text, `Hello ${name}`);
+                assert.equal(res.text, 'Hello,  Christina');
                 done();
             });
+    });
+
+    it('GET request returns with specified greeting Salutation', (done) => {
+        request.get('/greeting/Christina?salutation=yo')
+            .end( (err, res) => {
+                if(err) return done(err);
+                assert.equal(res.text, 'yo Christina');
+                done();
+            });
+
     });
 });
 
