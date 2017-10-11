13043a1f65422a9dc1ae52ba9087f6c9500519f0
erdem koral <koral.erdem@gmail.com>
2017-10-11 09:52:19 -0700

    initial set up is complete

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('../lib/app');
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 6000;
+
+server.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`server is running on port ${ port }`);
+});
