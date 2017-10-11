8de1eda485c90c5b808642605dad7c181e816c93
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-11 12:29:48 -0700

    I don;t know what I am doing

--- test/app-test.js
+++ test/app-test.js
@@ -1,3 +1,29 @@
-const http = require('http');
-const app = require('../lib/');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../lib/app');
+chai.use(chaiHttp);
+const assert = chai.assert;
+const greeting = require('../lib/greet-app');
 
+const request = chai.request(app);
+
+describe('hello-http app', () =>{
+
+    it('GET /name', () =>{
+        request.get('/name')
+            .end((err, res) =>{
+                assert.deepEqual(res.text, 'hello person');
+            });
+    });
+
+    // it('Returns a 404 message', () => {
+    //     request.get('/bad')
+    //         .end((res, res) =>{
+    //             if(!err)return done('error from http response');
+    //             assert.equal(err.status, 404);
+    //             assert.equal(res.text, `Cannot ${method}, ${url, path}`);
+    //             done();
+    //         });
+    // });
+
+});
