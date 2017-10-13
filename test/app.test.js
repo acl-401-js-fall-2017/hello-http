9a5d8e2845d53faa8dc59befc6242c0d487f2d17
mirrorapps <eli.speigel@gmail.com>
2017-10-13 11:33:43 -0700

    all passing

--- test/app.test.js
+++ test/app.test.js
@@ -7,12 +7,13 @@ const assert = chai.assert;
 const request = chai.request(app);
 
 describe('App', () => {
+   
     describe('Greeting', () => {
         const name = 'Eli';
         it('responds with greeting when GET /greeting/<name>', done => {
             request.get(`/greeting/${name}`)
                 .end((err, res) => {
-                    if (err) return done(err);
+                    if (err) done(err);
                     assert.equal(res.text, 'hello Eli');
                     done();
                 });
@@ -22,22 +23,29 @@ describe('App', () => {
             const salutation = 'yo';
             request.get(`/greeting/${name}?salutation=${salutation}`)
                 .end((err, res) => {
-                    if (err) return done(err);
+                    if (err) done(err);
                     assert.equal(res.text, 'yo Eli');
                     done();
                 });
         });
     });
-    describe('Interesting fact', () => {
+    describe('Interesting Fact', () => {
         it('responds to GET /fact with an http fact', done => {
             request.get('/fact')
                 .end((err, res) => {
-                    if (err) return done(err);
-                    assert.equal(res.fact, [
-                        'http typically uses port 80',
-                        'https is a more secure version of http',
-                        'https uses port'
-                    ]);
+                    if (err) done(err);
+                    assert.ok(res.text);
+                    done();
+                });
+        });
+    });
+    describe('Error Message', () => {
+        it('returns 404 when not found', done => {
+            request.get('/bad')
+                .end((err, res) => {
+                    if (!err) done('expected error from http response');
+                    assert.equal(err.status, 404);
+                    assert.equal(res.text, 'Cannot <Method> <Pathname>'); 
                     done();
                 });
         });
