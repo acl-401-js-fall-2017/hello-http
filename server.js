f3ca3a9e9ae80de31715aef82ae4f69d9079e5d7
ckrountree <jmail0207@gmail.com>
2017-10-13 18:01:54 -0700

    build up server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// const http = require('http');
-// const app = require('./lib/app');
+const http = require('http');
+const app = require('./lib/app');
 
-// const server = http.
+const server = http.createServer(app);
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+    console.log( 'server running on port', port); //eslint-disable-line
+});
