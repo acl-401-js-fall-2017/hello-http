c8ea4d6f28e44f57cc2745b6cf4df7dfc701a6a6
ckrountree <jmail0207@gmail.com>
2017-10-13 18:05:09 -0700

    make get method generic with template literal

--- test/app.test.js
+++ test/app.test.js
@@ -1,21 +1,26 @@
 const app = require('../lib/app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const http = require('http');
 const assert = chai.assert;
-// asking chai to take the app and test against it ( a fake server)
-const request = chai.request(app);
+chai.use(chaiHttp);
 
 describe('greet app', () => {
+    // const server = http.createServer(app);
+    // asking chai to take the app and test against it ( a fake server)
+    // const request = chai.request(server);
+
+    // after(done => server.close(done));
     it('sends a request to get greeting', done => {
         // send a request with input = '/greet/kate'
         // '/greet/kate' is a string with the url path
-        request.get('/greet/kate')
+        const name = 'Kate';
+        request.get(`/greet/${ name }`)
             // .end() used to make the request and assert on its response
             .end((err, res) => {
                 if(err) return done(err);
                 assert.equal(res.text, 'Hello Kate');
                 done();
-
             });
 
     });
