ba96420f98ab4edd7fc8115b74fcce0f950d7686
ckrountree <jmail0207@gmail.com>
2017-10-16 17:48:14 -0700

    first test passing

--- server.js
+++ server.js
@@ -8,3 +8,4 @@ const port = process.env.PORT || 8080;
 server.listen(port, () => {
     console.log( 'server running on port', port); //eslint-disable-line
 });
+
--- test/app.test.js
+++ test/app.test.js
@@ -6,20 +6,18 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 
 describe('greet app', () => {
-    // const server = http.createServer(app);
-    // asking chai to take the app and test against it ( a fake server)
-    // const request = chai.request(server);
+    const server = http.createServer(app);
+    const request = chai.request(server);
 
-    // after(done => server.close(done));
+    after(done => server.close(done));
+
     it('sends a request to get greeting', done => {
-        // send a request with input = '/greet/kate'
-        // '/greet/kate' is a string with the url path
         const name = 'Kate';
         request.get(`/greet/${ name }`)
             // .end() used to make the request and assert on its response
             .end((err, res) => {
                 if(err) return done(err);
-                assert.equal(res.text, 'Hello Kate');
+                assert.equal(res.text, 'hello Kate');
                 done();
             });
 
